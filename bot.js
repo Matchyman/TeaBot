2b9ce96196d2f1adb1216fd2ae06b8b610dc8749
Matchyman <33900044+Matchyman@users.noreply.github.com>
2019-12-27 15:06:11 +0000

    Add files via upload

--- bot.js
+++ bot.js
@@ -25,46 +25,6 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
-
-
-// Called every time a message comes in
-function onMessageHandler(target, context, msg, self,) {
-    if (self) { return; } // Ignore messages from the bot
-
-    // Remove whitespace from chat message
-    const commandName = msg.trim();
-
-    
-
-    // If the command is known, let's execute it
-    if (commandName === '!dice') {
-        const num = rollDice();
-        client.say(target, `You rolled a ${num}`);
-        console.log(`* Executed ${commandName} command`);
-    } else {
-        console.log(`* Unknown command ${commandName}`);
-    }
-
-    // Hello messages
-    if (msg.toLowerCase().startsWith(`hello`) === true) {
-        client.say(target, `Hello ${context.username}`);
-    }
-    if (msg.toLowerCase().startsWith(`hi`) === true) {
-        client.say(target, `Hi ${context.username}`);
-    }
-    if (msg.toLowerCase().startsWith(`hey`) === true) {
-        client.say(target, `Hey ${context.username}`);
-    }
-
-    if (msg.toLowerCase().startsWith(`!ball`) === true)
-    {
-        const saying = ball();
-        client.say(target, `${context.username} ${saying}`);
-    }
-
-}
-
-// Function called when the "dice" command is issued
 function rollDice() {
     const sides = 6;
     return Math.floor(Math.random() * sides) + 1;
@@ -101,14 +61,88 @@ function ball() {
             break
         default:
             return result;
-        
+
     }
 }
 
-// Called every time the bot connects to Twitch chat 
+function shoutout(msg) {
+    const a = msg.slice(0, 3);
+    return a;
+}
+
+
+// Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
 }
+
+// Called every time a message comes in
+function onMessageHandler(target, context, msg, self,) {
+    if (self) { return; } // Ignore messages from the bot
+
+    // Remove whitespace from chat message
+    const commandName = msg.trim();
+
+    // If the command is known, let's execute it
+    if (commandName === '!dice') {
+        const num = rollDice();
+        client.say(target, `You rolled a ${num}`);
+        console.log(`* Executed ${commandName} command`);
+    } else {
+        console.log(`* Unknown command ${commandName}`);
+    }
+
+    //Shoutout command using slice to get username
+    if (msg.toLowerCase().startsWith(`!so`) === true) {
+        const name = shoutout(msg);
+        client.say(`Boo ${ msg } ${name}`);
+        console.log(`* Executed ${commandName} command`);
+    } else {
+        console.log(`* Unknown command ${commandName}`);
+    }
+
+    // Pack name
+    if (commandName === `!pack`) {
+        client.say(target, ` This pack is Engineer's Life, you can find it on the Twitch Launcher`)
+        console.log(`* Executed ${commandName} command`);
+    } else {
+        console.log(`* Unknown command ${commandName}`);
+    }
+    if (msg.toLowerCase().startsWith(`!ball`) === true) {
+        const saying = ball();
+        client.say(target, `${context.username} ${saying}`);
+    }
+    
+    // Hello messages
+    /*if (msg.toLowerCase().startsWith(`hello`) === true) {
+        client.say(target, `Hello ${context.username}`);
+    }
+    if (msg.toLowerCase().startsWith(`hi`) === true) {
+        client.say(target, `Hi ${context.username}`);
+    }
+    if (msg.toLowerCase().startsWith(`hey`) === true) {
+        client.say(target, `Hey ${context.username}`);
+    }
+
+    
+    */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Follower to channel
 client.on("follower", (channel,username) => {
@@ -123,7 +157,7 @@ client.on("subscription", (channel, username, method, message, userstate) => {
 //Resub to channel
 client.on("resub", (channel, username, months, message, userstate, methods) => {
     let cumulativeMonths = ~~userstate["msg-param-cumulative-months"];
-    client.say(channel, `${username} Thanks for resubscribing for ${months} months enjoy more tea`);
+    client.say(channel, `${username} Thanks for resubscribing for ${cumulativeMonths} months enjoy more tea`);
 });
 // Gifted sub to channel
 
@@ -149,7 +183,9 @@ client.on("hosted", (channel, username, viewers, autohost) => {
 // Raid from user
 client.on("raided", (channel, username, viewers) => {
     client.say(channel, `${username} is raiding the channel with ${viewers} viewers, the kettle has boiled plenty of tea for everone!`);
+    client.say(channel, `Go follow ${username} at twitch.tv/${username}`);
 });
+
 // Continuing gift sub recieved
 client.on("anongiftpaidupgrade", (channel, username, userstate) => {
     client.say(channel, `${username} is continuing their sub to the channel, Thank you very much`);
